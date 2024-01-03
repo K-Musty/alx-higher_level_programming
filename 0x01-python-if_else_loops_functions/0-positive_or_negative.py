b32d991c332dacbcfbaaedb25df9173f97af3336
K-Musty <kmustapha9564@gmail.com>
2024-01-02 16:46:48 -0800

    This commit would fix errors

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(number)
+if number > 0:
+    print(numbr, "is positive")
+elif number == 0:
+    priint(number, "is zero")
+else:
+    print(number, "is negative")

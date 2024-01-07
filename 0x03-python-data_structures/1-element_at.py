4f90edd1f4d331376c0e87bc3e606cdb6d66980d
K-Musty <kmustapha9564@gmail.com>
2024-01-07 15:09:11 -0800

    commit to fix error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return None
-
-    for k, i in enumerate(my_list):
-        if k == idx:
-            return i
-
+    size = len(my_list)
+    if (idx < 0 or idx > size - 1):
+        return (None)
+    for i, e in enumerate(my_list):
+        if (i == idx):
+            return (e)

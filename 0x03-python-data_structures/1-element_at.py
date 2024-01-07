02398d35dbd88c35d59edb41262d4f5d6f417300
K-Musty <kmustapha9564@gmail.com>
2024-01-07 15:01:00 -0800

    correction for task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx < 0 or idx > (len(my_list) -1)):
-        return (None)
+    if idx < 0 or idx > (len(my_list) -1):
+        return None
+
     for k, i in enumerate(my_list):
         if (k == idx):
             return (i)

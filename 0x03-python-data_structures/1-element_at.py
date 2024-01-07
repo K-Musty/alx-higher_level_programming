7b50a218aee8e1ff5b2b32d11857ee6ede8da15e
K-Musty <kmustapha9564@gmail.com>
2024-01-07 14:53:16 -0800

    commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if (idx < 0 or idx > (len(my_list) -1)):
         return None
     for k, i in enumerate(my_list):
-        if (i == idx):
+        if (k == idx):
             return (i)
 

29d254d6805679d935284d1e6da5ef0d44b76ad1
K-Musty <kmustapha9564@gmail.com>
2024-04-08 16:02:48 -0700

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-console.log(process.argv.length < 4 ? 0 : process.argv.slice(2).sort((a, b) => b - a)[1]);
+// script that searches the second biggest integer in the list of arguments
+const mySol = process.argv;
+if (mySol.length <= 3) {
+  console.log(0);
+} else {
+  console.log(mySol.sort((x, y) => y - x).slice(3)[0]);
+}
